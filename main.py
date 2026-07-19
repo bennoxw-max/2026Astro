1577b3d117e912ab85c21da4e83af8a790783566
bennoxw-max <bennoxw@gmail.com>
2026-07-19 10:06:28 +1000

    Enhance chemical spill handling with improved movement and sensor integration

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ import time
 motor_pair.pair(motor_pair.PAIR_1, port.D, port.C)
 
 
+
 async def chemical_spill():
+    print("FUNC: chemical_spill()")
     runloop.run(square_off())
 
     motion_sensor.reset_yaw(0)
     yaw = motion_sensor.tilt_angles()[0]/10
 
     await motor_pair.move_tank_for_degrees(motor_pair.PAIR_1, 540, 600, 600)
-    
+
     offset = time.ticks_ms()
     timer = time.ticks_ms() - offset
 
@@ -32,13 +34,157 @@ async def chemical_spill():
         ultrasonic_dist = distance_sensor.distance(port.E)
         yaw = motion_sensor.tilt_angles()[0]/10
         timer = time.ticks_ms() - offset
-
-    print("found can")
     motor_pair.stop(motor_pair.PAIR_1)
 
+    if timer > 6000 and (yaw > -3 and yaw < 3):
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 540, -600, -600 # Move backwards to sqaure off with the silver tape
+        ) # move back to silver tape
+        runloop.run(square_off()) # Square off with the silver tape
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 150, 600, 600
+        ) # move forward 6 cm
+        motor.run_for_degrees(port.C, 216, velocity=500)
+        motor.reset_relative_position(port.C, 0)
+
+        ultrasonic_dist = distance_sensor.distance(port.E)
+        while ((ultrasonic_dist) > 270 or (ultrasonic_dist) < 0):
+            motor_pair.move(motor_pair.PAIR_1, 18, velocity=300) # Keep on repeating donut action until ultrasonic sees the can
+            ultrasonic_dist = distance_sensor.distance(port.E)
+        motor_pair.stop(motor_pair.PAIR_1)
+
+        donut_degree = motor.relative_position(port.C)
+
+        motor.reset_relative_position(port.C, 0)
+
+        left_col = color_sensor.color(port.B)
+        right_col = color_sensor.color(port.A)
+        while (left_col is not color.WHITE) or (right_col is not color.WHITE):
+            motor_pair.move(motor_pair.PAIR_1, 0, velocity=300) # Keep moving forward (pushing the can out of the oil spill) until both colour sensors see white
+            left_col = color_sensor.color(port.B)
+            right_col = color_sensor.color(port.A)
+        motor_pair.stop(motor_pair.PAIR_1)
+
+        while (motor.relative_position(port.C) < 1):
+            motor_pair.move(motor_pair.PAIR_1, 0, velocity=-300) # Reverse to the same spot where we found the can (the position where the wheel position was reset)
+        motor_pair.stop(motor_pair.PAIR_1)
+
+        motor.reset_relative_position(port.C, donut_degree) # Restore degree of the wheel to the position where we found the can so we can reverse the donut action
+
+        while (motor.relative_position(port.C) >= -50):
+            motor_pair.move(motor_pair.PAIR_1, 18, velocity=-500)
+        motor_pair.stop(motor_pair.PAIR_1)
+
+        runloop.run(turn_to_yaw(0, 300))
+        runloop.run(square_off())
+        runloop.run(turn_to_yaw(179, 200))
+
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 80, 400, 400
+        )
+        runloop.run(turn_to_yaw(-90, 300))
+
+        # REVIEW //////
+
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 240, -600, -600
+        )
+
+        # move forward until either colour sensor detects black
+        left_col = color_sensor.color(port.B)
+        right_col = color_sensor.color(port.A)
+        while (left_col is not color.BLACK) and (right_col is not color.BLACK):
+            motor_pair.move(motor_pair.PAIR_1, 0, velocity=300)
+            left_col = color_sensor.color(port.B)
+            right_col = color_sensor.color(port.A)
+        motor_pair.stop(motor_pair.PAIR_1)
+
+        # move forward 3 cm
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 90, 300, 300
+        )
+
+        runloop.run(turn_to_yaw(179, 200))
+
+        # move backwards 3 cm
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 90, -600, -600
+        )
+
+    else:
+        # motor_pair.stop(motor_pair.PAIR_1)
+        motor.reset_relative_position(port.C, 0) # Reset the relative position
+
+        # move forward until either sensor detects white
+        left_col = color_sensor.color(port.B)
+        right_col = color_sensor.color(port.A)
+        while (left_col is not color.WHITE) and (right_col is not color.WHITE):
+            motor_pair.move_tank(motor_pair.PAIR_1, 500, 500)
+            left_col = color_sensor.color(port.B)
+            right_col = color_sensor.color(port.A)
+        motor_pair.stop(motor_pair.PAIR_1)
+
+        # reverse to the position where the wheel position was reset
+        while (motor.relative_position(port.C) > 5):
+            motor_pair.move_tank(motor_pair.PAIR_1, -500, -500)
+        motor_pair.stop(motor_pair.PAIR_1)
+
+        # rotate until the robot's yaw is close to zero -------------------------- REPLACED WITH TURN TO YAW COMMAND
+        #yaw = motion_sensor.tilt_angles()[0] / 10
+        #while not (-2 <= yaw <= 2):
+        #    motor_pair.move_tank(motor_pair.PAIR_1, 150, -150)
+        #    yaw = motion_sensor.tilt_angles()[0] / 10
+        #motor_pair.stop(motor_pair.PAIR_1)
+        runloop.run(turn_to_yaw(0, 200))
+
+        # reverse until either colour sensor's raw red value exceeds 500
+        left_rr = color_sensor.rgbi(port.B)[0]
+        right_rr = color_sensor.rgbi(port.A)[0]
+        while (left_rr <= 500) and (right_rr <= 500):
+            motor_pair.move_tank(motor_pair.PAIR_1, -500, -500)
+            left_rr = color_sensor.rgbi(port.B)[0]
+            right_rr = color_sensor.rgbi(port.A)[0]
+        motor_pair.stop(motor_pair.PAIR_1)
+
+        runloop.run(square_off())
+        runloop.run(turn_to_yaw(179, 200))
+
+        # move forward 4 cm
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 120, 300, 300
+        )
+
+        runloop.run(turn_to_yaw(-90, 300))
+
+        # move backwards 8 cm
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 240, -300, -300
+        )
+
+        # move forward until either sensor detects black
+        left_col = color_sensor.color(port.B)
+        right_col = color_sensor.color(port.A)
+        while (left_col is not color.BLACK) and (right_col is not color.BLACK):
+            motor_pair.move(motor_pair.PAIR_1, 0, velocity=300)
+            left_col = color_sensor.color(port.B)
+            right_col = color_sensor.color(port.A)
+        motor_pair.stop(motor_pair.PAIR_1)
+
+        # move forward 3 cm
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 90, 400, 400
+        )
+
+        runloop.run(turn_to_yaw(179, 200))
+
+        # move backwards 3 cm
+        await motor_pair.move_tank_for_degrees(
+            motor_pair.PAIR_1, 90, -300, -300
+        )
 
 
 async def square_off():
+    print("FUNC: square_off()")
     offset = time.ticks_ms()
     timer = time.ticks_ms() - offset
 
@@ -58,13 +204,12 @@ async def square_off():
 
         timer = time.ticks_ms() - offset
 
-
     motor.stop(port.C)
     motor.stop(port.D)
 
 
 async def turn_to_yaw(target_yaw, speed):
-    print("FUNC: TURN_TO_YAW()")
+    print("FUNC: turn_to_yaw()")
     yaw = motion_sensor.tilt_angles()[0]/10
     aim = -target_yaw
 
@@ -72,67 +217,97 @@ async def turn_to_yaw(target_yaw, speed):
 
     if yaw < aim:
         while (yaw < (aim - 3)) or (yaw > (aim + 3)):
-            motor_pair.move_tank(motor_pair.PAIR_1, -speed, speed)
+            motor_pair.move_tank(
+                motor_pair.PAIR_1, -speed, speed
+            )
             yaw = motion_sensor.tilt_angles()[0]/10
-
-
         motor_pair.stop(motor_pair.PAIR_1)
+
     else:
         while (yaw < (aim - 3)) or (yaw > (aim + 3)):
-            motor_pair.move_tank(motor_pair.PAIR_1, speed, -speed)
+            motor_pair.move_tank(
+                motor_pair.PAIR_1, speed, -speed
+            )
             yaw = motion_sensor.tilt_angles()[0]/10
-
-
         motor_pair.stop(motor_pair.PAIR_1)
 
+
 async def left_green_turn():
+    print("FUNC: left_green_turn()")
+
     # go forward
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 120, 0, velocity=280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 120, 0, velocity=280
+    )
 
     # turn left
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 90, -100, velocity=280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 90, -100, velocity=280
+    )
 
     # turn left until right sensor sees black
     right_ref = color_sensor.reflection(port.A)
     while (right_ref > 40):
         right_ref = color_sensor.reflection(port.A)
-        motor_pair.move(motor_pair.PAIR_1, -100, velocity=280)
+        motor_pair.move(
+            motor_pair.PAIR_1, -100, velocity=280
+        )
 
     # realign with the line
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 40, 100, velocity=280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 40, 100, velocity=280
+    )
 
     # go forward
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 50, 0, velocity=280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 50, 0, velocity=280
+    )
 
 async def right_green_turn():
+    print("FUNC: right_green_turn()")
+
     # go forward
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 120, 0, velocity=280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 120, 0, velocity=280
+    )
 
     # turn right
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 90, 100, velocity=280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 90, 100, velocity=280
+    )
 
     # turn right until left sensor sees black
     left_ref = color_sensor.reflection(port.B)
     while (left_ref > 40):
         left_ref = color_sensor.reflection(port.B)
-        motor_pair.move(motor_pair.PAIR_1, 100, velocity=280)
+        motor_pair.move(
+            motor_pair.PAIR_1, 100, velocity=280
+        )
 
     # realign with theline
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 40, -100, velocity=280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 40, -100, velocity=280
+    )
 
     # go forward
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 50, 0, velocity=280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 50, 0, velocity=280
+    )
 
 async def bottle():
-    print("bottle")
+    print("FUNC: bottle()")
 
     motion_sensor.reset_yaw(0)
 
     # go backwards
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 150, 0, velocity=-280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 150, 0, velocity=-280
+    )
 
     # turn right
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 260, 40, velocity=300)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 260, 40, velocity=300
+    )
 
     # arc around the bottle
     right_col = color_sensor.color(port.A)
@@ -142,12 +317,11 @@ async def bottle():
         right_col = color_sensor.color(port.A)
 
     # go forward
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, 50, 0, velocity=280)
+    await motor_pair.move_for_degrees(
+        motor_pair.PAIR_1, 50, 0, velocity=280
+    )
 
     runloop.run(turn_to_yaw(0, 200))
-
-
-
 
 
 async def main():
